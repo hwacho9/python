b0d25beb05ab284a1dc9be94b38e08d9e8c0b9cc
hwacho9 <63436365+hwacho9@users.noreply.github.com>
2021-02-03 20:27:40 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,135 +1,173 @@
-import os
+import pandas as pd
+from bs4 import BeautifulSoup
+import pymysql
+import calendar
+import time
+import json
+import requests
+from datetime import datetime
+from threading import Timer
 
 
-def add_to_dict(a_dict={}, Key="", Value=""):
-    if type(a_dict) is not dict:
-        print("You need to send a dictionary. You sent:", type(a_dict))
-    elif not Key or not Value:
-        print("You need to send a word and a definition")
-    elif Key in a_dict:
-        print(f"{Key} is aready on the dictionary. Won't add.")
-    else:
-        a_dict[Key] = Value
-        print(f"{Key} has been added")
+class DBUpdater:
+    def __init__(self):
+        """생성자: MariaDB 연결 및 종목코드 딕셔너리 생성"""
+        self.conn = pymysql.connect(host='localhost', user='root',
+                                    password='myPa$$word', db='INVESTAR', charset='utf8')
+
+        with self.conn.cursor() as curs:
+            sql = """
+            CREATE TABLE IF NOT EXISTS company_info (
+                code VARCHAR(20),
+                company VARCHAR(40),
+                last_update DATE,
+                PRIMARY KEY (code))
+            """
+            curs.execute(sql)
+            sql = """
+            CREATE TABLE IF NOT EXISTS daily_price (
+                code VARCHAR(20),
+                date DATE,
+                open BIGINT(20),
+                high BIGINT(20),
+                low BIGINT(20),
+                close BIGINT(20),
+                diff BIGINT(20),
+                volume BIGINT(20),
+                PRIMARY KEY (code, date))
+            """
+            curs.execute(sql)
+        self.conn.commit()
+        self.codes = dict()
+
+    def __del__(self):
+        """소멸자: MariaDB 연결 해제"""
+        self.conn.close()
+
+    def read_krx_code(self):
+        """KRX로부터 상장기업 목록 파일을 읽어와서 데이터프레임으로 반환"""
+        url = 'http://kind.krx.co.kr/corpgeneral/corpList.do?method='\
+            'download&searchType=13'
+        krx = pd.read_html(url, header=0)[0]
+        krx = krx[['종목코드', '회사명']]
+        krx = krx.rename(columns={'종목코드': 'code', '회사명': 'company'})
+        krx.code = krx.code.map('{:06d}'.format)
+        return krx
+
+    def update_comp_info(self):
+        """종목코드를 company_info 테이블에 업데이트 한 후 딕셔너리에 저장"""
+        sql = "SELECT * FROM company_info"
+        df = pd.read_sql(sql, self.conn)
+        for idx in range(len(df)):
+            self.codes[df['code'].values[idx]] = df['company'].values[idx]
+
+        with self.conn.cursor() as curs:
+            sql = "SELECT max(last_update) FROM company_info"
+            curs.execute(sql)
+            rs = curs.fetchone()
+            today = datetime.today().strftime('%Y-%m-%d')
+            if rs[0] == None or rs[0].strftime('%Y-%m-%d') < today:
+                krx = self.read_krx_code()
+                for idx in range(len(krx)):
+                    code = krx.code.values[idx]
+                    company = krx.company.values[idx]
+                    sql = f"REPLACE INTO company_info (code, company, last"\
+                        f"_update) VALUES ('{code}', '{company}', '{today}')"
+                    curs.execute(sql)
+                    self.codes[code] = company
+                    tmnow = datetime.now().strftime('%Y-%m-%d %H:%M')
+                    print(f"[{tmnow}] #{idx+1:04d} REPLACE INTO company_info "
+                          f"VALUES ({code}, {company}, {today})")
+                self.conn.commit()
+                print('')
+
+    def read_naver(self, code, company, pages_to_fetch):
+        """네이버에서 주식 시세를 읽어서 데이터프레임으로 반환"""
+        try:
+            url = f"http://finance.naver.com/item/sise_day.nhn?code={code}"
+            html = BeautifulSoup(requests.get(url,
+                                              headers={'User-agent': 'Mozilla/5.0'}).text, "lxml")
+            pgrr = html.find("td", class_="pgRR")
+            if pgrr is None:
+                return None
+            s = str(pgrr.a["href"]).split('=')
+            lastpage = s[-1]
+            df = pd.DataFrame()
+            pages = min(int(lastpage), pages_to_fetch)
+            for page in range(1, pages + 1):
+                pg_url = '{}&page={}'.format(url, page)
+                df = df.append(pd.read_html(requests.get(pg_url,
+                                                         headers={'User-agent': 'Mozilla/5.0'}).text)[0])
+                tmnow = datetime.now().strftime('%Y-%m-%d %H:%M')
+                print('[{}] {} ({}) : {:04d}/{:04d} pages are downloading...'.
+                      format(tmnow, company, code, page, pages), end="\r")
+            df = df.rename(columns={'날짜': 'date', '종가': 'close', '전일비': 'diff',
+                                    '시가': 'open', '고가': 'high', '저가': 'low', '거래량': 'volume'})
+            df['date'] = df['date'].replace('.', '-')
+            df = df.dropna()
+            df[['close', 'diff', 'open', 'high', 'low', 'volume']] = df[['close',
+                                                                         'diff', 'open', 'high', 'low', 'volume']].astype(int)
+            df = df[['date', 'open', 'high', 'low', 'close', 'diff', 'volume']]
+        except Exception as e:
+            print('Exception occured :', str(e))
+            return None
+        return df
+
+    def replace_into_db(self, df, num, code, company):
+        """네이버에서 읽어온 주식 시세를 DB에 REPLACE"""
+        with self.conn.cursor() as curs:
+            for r in df.itertuples():
+                sql = f"REPLACE INTO daily_price VALUES ('{code}', "\
+                    f"'{r.date}', {r.open}, {r.high}, {r.low}, {r.close}, "\
+                    f"{r.diff}, {r.volume})"
+                curs.execute(sql)
+            self.conn.commit()
+            print('[{}] #{:04d} {} ({}) : {} rows > REPLACE INTO daily_'
+                  'price [OK]'.format(datetime.now().strftime('%Y-%m-%d'
+                                                              ' %H:%M'), num+1, company, code, len(df)))
+
+    def update_daily_price(self, pages_to_fetch):
+        """KRX 상장법인의 주식 시세를 네이버로부터 읽어서 DB에 업데이트"""
+        for idx, code in enumerate(self.codes):
+            df = self.read_naver(code, self.codes[code], pages_to_fetch)
+            if df is None:
+                continue
+            self.replace_into_db(df, idx, code, self.codes[code])
+
+    def execute_daily(self):
+        """실행 즉시 및 매일 오후 다섯시에 daily_price 테이블 업데이트"""
+        self.update_comp_info()
+
+        try:
+            with open('config.json', 'r') as in_file:
+                config = json.load(in_file)
+                pages_to_fetch = config['pages_to_fetch']
+        except FileNotFoundError:
+            with open('config.json', 'w') as out_file:
+                pages_to_fetch = 100
+                config = {'pages_to_fetch': 1}
+                json.dump(config, out_file)
+        self.update_daily_price(pages_to_fetch)
+
+        tmnow = datetime.now()
+        lastday = calendar.monthrange(tmnow.year, tmnow.month)[1]
+        if tmnow.month == 12 and tmnow.day == lastday:
+            tmnext = tmnow.replace(year=tmnow.year+1, month=1, day=1,
+                                   hour=17, minute=0, second=0)
+        elif tmnow.day == lastday:
+            tmnext = tmnow.replace(month=tmnow.month+1, day=1, hour=17,
+                                   minute=0, second=0)
+        else:
+            tmnext = tmnow.replace(day=tmnow.day+1, hour=17, minute=0,
+                                   second=0)
+        tmdiff = tmnext - tmnow
+        secs = tmdiff.seconds
+        t = Timer(secs, self.execute_daily)
+        print("Waiting for next update ({}) ... ".format(tmnext.strftime
+                                                         ('%Y-%m-%d %H:%M')))
+        t.start()
 
 
-def get_from_dict(a_dict={}, Key=""):
-    if type(a_dict) is not dict:
-        print("You need to send a dictionary. You sent:", type(a_dict))
-    elif not Key:
-        print("Youd need to send a word to search for.")
-    elif Key in a_dict:
-        print(f"{Key} :", a_dict[Key])
-    else:
-        print(f"{Key} was not found in this dict")
-
-
-def update_word(a_dict={}, Key="", Value=""):
-    if type(a_dict) is not dict:
-        print("You need to send a dictionary. You sent:", type(a_dict))
-    elif not Key or not Value:
-        print("You need to send a word and a definition to update.")
-    elif Key not in a_dict:
-        print(f"{Key} is not on the dict. Can't update non-existing word.")
-    else:
-        a_dict[Key] = Value
-        print(f"{Key} has been updated to : {Value}")
-    pass
-
-
-def delete_from_dict(a_dict={}, Key=""):
-    if type(a_dict) is not dict:
-        print("You need to send a dictionary. You sent:", type(a_dict))
-    elif not Key:
-        print("You need to specify a word to delete.")
-    elif Key not in a_dict:
-        print(f"{Key} is not in this dict. Won't delete.")
-    else:
-        del a_dict[Key]
-        print(f"{Key} has been deleted.")
-    pass
-
-
-os.system('clear')
-
-my_english_dict = {}
-
-print("\n###### add_to_dict ######\n")
-
-# Should not work. First argument should be a dict.
-print('add_to_dict("hello", "kimchi"):')
-add_to_dict("hello", "kimchi")
-
-# Should not work. Definition is required.
-print('\nadd_to_dict(my_english_dict, "kimchi"):')
-add_to_dict(my_english_dict, "kimchi")
-
-# Should work.
-print('\nadd_to_dict(my_english_dict, "kimchi", "The source of life."):')
-add_to_dict(my_english_dict, "kimchi", "The source of life.")
-
-# Should not work. kimchi is already on the dict
-print('\nadd_to_dict(my_english_dict, "kimchi", "My fav. food"):')
-add_to_dict(my_english_dict, "kimchi", "My fav. food")
-
-print("\n\n###### get_from_dict ######\n")
-
-# Should not work. First argument should be a dict.
-print('\nget_from_dict("hello", "kimchi"):')
-get_from_dict("hello", "kimchi")
-
-# Should not work. Word to search from is required.
-print('\nget_from_dict(my_english_dict):')
-get_from_dict(my_english_dict)
-
-# Should not work. Word is not found.
-print('\nget_from_dict(my_english_dict, "galbi"):')
-get_from_dict(my_english_dict, "galbi")
-
-# Should work and print the definiton of 'kimchi'
-print('\nget_from_dict(my_english_dict, "kimchi"):')
-get_from_dict(my_english_dict, "kimchi")
-
-print("\n\n###### update_word ######\n")
-
-# Should not work. First argument should be a dict.
-print('\nupdate_word("hello", "kimchi"):')
-update_word("hello", "kimchi")
-
-# Should not work. Word and definiton are required.
-print('\nupdate_word(my_english_dict, "kimchi"):')
-update_word(my_english_dict, "kimchi")
-
-# Should not work. Word not found.
-print('\nupdate_word(my_english_dict, "galbi", "Love it."):')
-update_word(my_english_dict, "galbi", "Love it.")
-
-# Should work.
-print('\nupdate_word(my_english_dict, "kimchi", "Food from the gods."):')
-update_word(my_english_dict, "kimchi", "Food from the gods.")
-
-# Check the new value.
-print('\nget_from_dict(my_english_dict, "kimchi"):')
-get_from_dict(my_english_dict, "kimchi")
-
-print("\n\n###### delete_from_dict ######\n")
-
-# Should not work. First argument should be a dict.
-print('\ndelete_from_dict("hello", "kimchi"):')
-delete_from_dict("hello", "kimchi")
-
-# Should not work. Word to delete is required.
-print('\ndelete_from_dict(my_english_dict):')
-delete_from_dict(my_english_dict)
-
-# Should not work. Word not found.
-print('\ndelete_from_dict(my_english_dict, "galbi"):')
-delete_from_dict(my_english_dict, "galbi")
-
-# Should work.
-print('\ndelete_from_dict(my_english_dict, "kimchi"):')
-delete_from_dict(my_english_dict, "kimchi")
-
-# Check that it does not exist
-print('\nget_from_dict(my_english_dict, "kimchi"):')
-get_from_dict(my_english_dict, "kimchi")
+if __name__ == '__main__':
+    dbu = DBUpdater()
+    dbu.execute_daily()
