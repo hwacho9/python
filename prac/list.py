86341917a9731f2665efef4a5a1333badac0f6d6
hwacho9 <63436365+hwacho9@users.noreply.github.com>
2021-02-03 20:26:29 +0900

    Create list.py

---
+++ prac/list.py
@@ -0,0 +1,6 @@
+small_birds = ['hummingbird', 'finch']
+extinct_bird = ['dodo', 'passenger pigeon', 'Norwegian Blue']
+carol_birds = [3, 'French hens', 2, 'turtledoves']
+all_birds = [small_birds, extinct_bird, 'macaw', carol_birds]
+
+print(all_birds[1][0])
